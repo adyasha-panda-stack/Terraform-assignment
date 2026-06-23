3eba84b261716ef6f8e5d30da07c3840b2a96dfa
adyasha-panda-stack <adyashapnd61@gmail.com>
2026-06-23 19:20:12 +0530

    Update main.tf

--- Terraform-1/main.tf
+++ Terraform-1/main.tf
@@ -1,1 +1,18 @@
 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e38835daf6b8a2b9"
+  instance_type = var.instance_type
+
+
+  tags = {
+    Name = "web-server"
+  }
+}
